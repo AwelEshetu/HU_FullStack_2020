2313653dd5ddc956a7e558b5b792f9338f5b0b44
Awel Eshetu <awel@arctechno.com>
2020-08-20 12:25:25 +0300

    unicafe 1.7

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({text,handleClick}) =><button onClick={handleClick}>{text}</button>;
-const States= ({text,count}) => <p>{text} {count}</p>
+const States= ({text,count}) => <p>{text} {count}</p>;
+const Feedback =({text,good,neutral,bad}) =>{
 
+ const stat= text==='all' ? `${good-bad}` : text==='average' ? `${(good-bad)/(good+bad+neutral)}` : `${(good / (good+bad+neutral)) * 100 }%`;
+
+  return (<p>{text} {(bad+good+neutral) !==0 ? stat : null} </p>)
+      
+}
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+ 
   
   const handleGood= () => {
     return setGood(good+1);
@@ -19,6 +26,9 @@ const App = () => {
   const handleBad= () => {
     return setBad(bad+1);
   }
+
+  
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -32,6 +42,9 @@ const App = () => {
         <States text='good' count={good}/>
         <States text='neutral' count={neutral}/>
         <States text='bad' count={bad}/>
+        <Feedback text='all' good={good} neutral={neutral} bad={bad} />
+        <Feedback text='average'  good={good} neutral={neutral} bad={bad} />
+        <Feedback text='positive'  good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
